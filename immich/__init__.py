a516459e8dae1b4208a9640d9dab380f16e4a713
timonrieger <mail@timonrieger.de>
2026-01-31 12:05:42 +0100

    fix: last release to the 'immich' namespace indicating the renaming to 'immichpy'

--- immich/__init__.py
+++ immich/__init__.py
@@ -1,3 +1,4 @@
-from immich.client.main import AsyncClient
-
-__all__ = ["AsyncClient"]
+raise ImportError(
+    "The 'immich' package has been renamed to 'immichpy'. "
+    "Install with: pip install immichpy"
+)
